27e902ed02d88affb2937eb0bfa9c3eec39970f9
rusefillc <sdfsdfqsf2334234234>
2023-06-28 18:44:53 -0400

    only https://github.com/rusefi/rusefi-hardware/issues/212

--- misc/stm32f1_test_project/spi.cpp
+++ misc/stm32f1_test_project/spi.cpp
@@ -8,9 +8,9 @@
 #define SPI_CS_PIN 4
 
 /* Low speed SPI configuration (281.250kHz, CPHA=0, CPOL=0, MSb first).*/
-static SPIConfig ls_spicfg = {false, NULL, SPI_CS_PORT, SPI_CS_PIN,
-                              SPI_CR1_BR_2 | SPI_CR1_BR_1,
-                              0};
+//static SPIConfig ls_spicfg = {false, NULL, SPI_CS_PORT, SPI_CS_PIN,
+//                              SPI_CR1_BR_2 | SPI_CR1_BR_1,
+//                              0};
 
 /*
  * SPI TX and RX buffers.
@@ -22,9 +22,9 @@ void InitSpi() {
   /*
    * SPI1 I/O pins setup.
    */
-  palSetPadMode(GPIOA, 5, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* SCK. */
-  palSetPadMode(GPIOA, 6, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MISO.*/
-  palSetPadMode(GPIOA, 7, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MOSI.*/
-  palSetPadMode(SPI_CS_PORT, SPI_CS_PIN, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPad(SPI_CS_PORT, SPI_CS_PIN);
+//  palSetPadMode(GPIOA, 5, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* SCK. */
+//  palSetPadMode(GPIOA, 6, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MISO.*/
+//  palSetPadMode(GPIOA, 7, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MOSI.*/
+//  palSetPadMode(SPI_CS_PORT, SPI_CS_PIN, PAL_MODE_OUTPUT_PUSHPULL);
+//  palSetPad(SPI_CS_PORT, SPI_CS_PIN);
 }
